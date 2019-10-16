a05805fd82a2b14fb402eaa40ea1b38f49097d32
sebastian@mclinica.com <sebastian@mclinica.com>
2019-10-16 23:49:48 +0800

    Initialized speech recognition

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:speech_recognition/speech_recognition.dart';
 
 void main() => runApp(MyApp());
+
+//Variables
+SpeechRecognition _speechRecognition;
+bool _isAvailable = false;
+bool _isListening = false;
+String resText = "";
 
 class MyApp extends StatelessWidget {
   @override
@@ -21,6 +28,21 @@ class Body extends StatefulWidget{
 
 class BodyState extends State<Body>{
   @override
+  void initState(){
+    super.initState();
+    initSpeechRecognizer();
+  }
+
+  void initSpeechRecognizer(){
+    _speechRecognition = SpeechRecognition();
+    _speechRecognition.setAvailabilityHandler((bool res) => setState(() => _isAvailable = res));
+    _speechRecognition.setRecognitionStartedHandler(() => setState(() => _isListening = true));
+    _speechRecognition.setRecognitionResultHandler((String speech) => setState(() => resText = speech));
+    _speechRecognition.setRecognitionCompleteHandler(() => setState(() => _isListening = false));
+    _speechRecognition.activate().then((res) => setState(() => _isAvailable = res));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -32,5 +54,22 @@ class BodyState extends State<Body>{
 }
 
 Widget _buildBody(){
-  return null;
+  return Container(
+    child: Column(
+      children: <Widget>[
+        _floatingButton(),
+      ],
+    )
+  );
 }
+
+Widget _floatingButton(){
+  return FloatingActionButton(
+    onPressed: (){
+      resText = "Clicked";
+      print(resText + "haha");
+    },
+  );
+}
+
+
