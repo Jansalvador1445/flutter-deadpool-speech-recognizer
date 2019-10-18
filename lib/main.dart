cea9e8d44bba23e5ce0387754385d1958e522d02
sebastian@mclinica.com <sebastian@mclinica.com>
2019-10-18 13:37:22 +0800

    Outline Button, Deadpool Image

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,17 @@ void main() => runApp(MyApp());
 SpeechRecognition _speechRecognition;
 bool _isAvailable = false;
 bool _isListening = false;
-String resText = "";
+String resText = "I'm Deadpool!";
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'flutter-speech-recognition',
+//      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primaryColor: Colors.green
+        primaryColor: Color(0xff1abc9c),
+        backgroundColor: Color(0xff2c3e50),
       ),
       home: Body()
     );
@@ -46,28 +49,65 @@ class BodyState extends State<Body>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Text Recognition'),
+        title: Text('Text Recognition',
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
       ),
+      backgroundColor: Color(0xff2c3e50),
       body: _buildBody(),
     );
   }
 
   Widget _buildBody(){
     return Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+        margin: EdgeInsets.only(bottom: 40),
+        child: new Stack(
           children: <Widget>[
-            _rowWidget(),
-            _textLabelResult(),
+            _deadPoolImage(),
+            Positioned(
+              child: new Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    _textLabelResult(),
+                    _clearTextLabelButton(),
+                    _rowWidget()
+                  ],
+                ),
+              )
+            )
           ],
         )
     );
   }
 
+  Widget _deadPoolImage(){
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          alignment: Alignment.center,
+          height: 500.0,
+          width: 300.0,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: ExactAssetImage('assets/deadpool.png'),
+              fit: BoxFit.fitWidth
+            )
+          ),
+        )
+      ],
+    );
+  }
+
   Widget _rowWidget(){
     return Row(
-      mainAxisAlignment: MainAxisAlignment.center ,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: <Widget>[
         _floatingButtonStop(),
         _floatingButtonMic(),
@@ -111,15 +151,38 @@ class BodyState extends State<Body>{
             }),
           );
         }
+        resText = "\nI'm Deadpool!";
       },
+    );
+  }
+
+  Widget _clearTextLabelButton(){
+    return Container(
+        margin: EdgeInsets.all(50),
+        child: OutlineButton(
+          child: Text('CLEAR TEXT'),
+          textColor: Colors.white,
+          borderSide: BorderSide(color: Colors.white),
+          onPressed: (){
+            setState(() {
+              resText = "I'm Deadpool!";
+            });
+          },
+        )
     );
   }
 
   Widget _textLabelResult(){
     return Container(
       padding: EdgeInsets.all(20),
-      margin: EdgeInsets.all(10),
-      child: Text(resText),
+      margin: EdgeInsets.all(20),
+      child: new Text(resText,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 30.0,
+        ),
+      ),
     );
   }
 
